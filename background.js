fc70ba74e5b52582360a89a5e65404c5cbf90600
rporteous1 <160448581+rporteous1@users.noreply.github.com>
2024-04-07 21:22:09 +1000

    Update background.js

--- background.js
+++ background.js
@@ -5,33 +5,37 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   });
   
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.contentScriptQuery === "fetchWeatherAndMarineData") {
-      const { lat, lon, todayDate } = request;
-      const weatherApiUrl = `https://api.weatherapi.com/v1/current.json?key=a72468e1e3234dc3b0543634242403&q=${lat},${lon}&aqi=no`;
-      const marineApiUrl = `https://api.weatherapi.com/v1/marine.json?key=a72468e1e3234dc3b0543634242403&q=${lat},${lon}`;
-      const forecastApiUrl = `https://api.weatherapi.com/v1/forecast.json?key=a72468e1e3234dc3b0543634242403&q=${lat},${lon}&dt=${todayDate}&days=1&aqi=no&alerts=no`;
 
-      Promise.all([
-        fetch(weatherApiUrl).then(response => response.json()),
-        fetch(marineApiUrl).then(response => response.json()),
-        fetch(forecastApiUrl).then(response => response.json())
-      ]).then(values => {
-        sendResponse({
-          weatherData: values[0],
-          marineData: values[1],
-          forecastData: values[2]
-        });
-      }).catch(error => {
-        console.error('Error fetching data:', error);
-        sendResponse({error: 'Failed to fetch data.'});
-      });
 
-      return true;  // indicates we will send a response asynchronously
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'fetchWeatherData') {
+        const { lat, lon } = request;
+        fetchWeatherData(lat, lon)
+            .then(data => sendResponse({ data }))
+            .catch(error => sendResponse({ error }));
+        return true; // Required to use sendResponse asynchronously
     }
-  }
-);
-
+ });
+ 
+ async function fetchWeatherData(lat, lon) {
+    const weatherApiUrl = `https://api.weatherapi.com/v1/current.json?key=a72468e1e3234dc3b0543634242403&q=${lat},${lon}&aqi=no`;
+    const todayDate = new Date().toISOString().split('T')[0];
+    const forecastApiUrl = `https://api.weatherapi.com/v1/forecast.json?key=a72468e1e3234dc3b0543634242403&q=${lat},${lon}&dt=${todayDate}&days=1&aqi=no&alerts=no`;
+ 
+    try {
+        const weatherResponse = await fetch(weatherApiUrl);
+        const weatherData = await weatherResponse.json();
+ 
+        if (weatherData && weatherData.current) {
+            const forecastResponse = await fetch(forecastApiUrl);
+            const forecastData = await forecastResponse.json();
+            return { weatherData, forecastData };
+        } else {
+            throw new Error('Unexpected API response structure');
+        }
+    } catch (error) {
+        throw error;
+    }
+ }
 
 // a72468e1e3234dc3b0543634242403
